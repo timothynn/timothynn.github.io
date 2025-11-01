c8ba353a5804d73f05edfca6d99ef16023f024b8
timothynn <timothynn08@gmail.com>
2025-11-01 11:50:25 +0300

    feat: restructure Next.js project layout and remove unused files

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import type { AppProps } from "next/app";
+import { Geist, Geist_Mono } from "next/font/google";
+import "../styles/globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <div className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <Component {...pageProps} />
+    </div>
+  );
+}
