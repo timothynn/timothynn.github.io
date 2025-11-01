6d1db82ecb530a150037dfb6c8a85d2fe7c29dce
timothynn <timothynn@users.noreply.github.com>
2025-11-01 09:05:03 +0000

    deploy: bd64871870043e386d398dd58fcb25eb3be20c04

---
+++ _next/static/t19VKI---4Fa8wH5DHFW_/_buildManifest.js
@@ -0,0 +1,18 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/cbf44a3ecfe89f2c.js"
+  ],
+  "/_error": [
+    "static/chunks/37996c94f38cf80d.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
